7be135945da3aae2e62fad58c0295a972792227c
jerzybalcer <jurekbalcer@gmail.com>
2023-01-14 19:38:55 +0100

    removed user

--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,6 +1,22 @@
-import { EditedItem, FullItem } from '../model';
+import { EditedItem, FullItem, NewItem, SimpleItem } from '../model';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export const getUserItems = async (): Promise<SimpleItem[]> =>
+    fetch(`${baseUrl}/items`).then((response) => response.json());
+
+export const getUserFavouriteItems = async (): Promise<SimpleItem[]> =>
+    fetch(`${baseUrl}/items/favourite`).then((response) => response.json());
+
+export const addUserItem = async (newItem: NewItem): Promise<Response> =>
+    fetch(`${baseUrl}/item`, {
+        method: 'POST',
+        body: JSON.stringify(newItem),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    });
 
 export const getItemDetails = async (itemId: string): Promise<FullItem> =>
     fetch(`${baseUrl}/item/${itemId}`).then((response) => response.json());
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,3 @@
-export * from './UserService';
 export * from './TagService';
 export * from './ItemService';
 export * from './CategoryService';
